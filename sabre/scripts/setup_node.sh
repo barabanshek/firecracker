d1b2034396316e6c4bb84248bc038f41882e7064
Nikita Lazarev <niklz@csail.mit.edu>
2024-04-17 19:25:03 -0700

    change

--- sabre/scripts/setup_node.sh
+++ sabre/scripts/setup_node.sh
@@ -9,7 +9,7 @@ echo 4000 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepag
 echo 4000 > /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
 
 # Configure IAA
-./configure_iaa_user.sh 0 1,3 16 8
+./configure_iaa_user.sh 0 1,15 16 8
 
 # Fix CPU frequencies
 ./prepare_machine.sh ${cpu_freq}
